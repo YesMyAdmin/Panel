922a28146f37fd1b7989affd413d8081a4180123
non_ke <a@a.com>
2026-06-09 12:34:43 +0800

    modify:一些样式上的调整

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,162 +1,169 @@
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table";
 
 import {
-    type ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
+  type ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
 } from "@tanstack/react-table";
-import {Input} from "./ui/input";
-import {Ellipsis, LucideScanSearch, Plus} from "lucide-react";
+import { Input } from "./ui/input";
+import { Ellipsis, LucideScanSearch, Plus } from "lucide-react";
 import {
-    Pagination,
-    PaginationContent,
-    PaginationItem,
-    PaginationPrevious,
-    PaginationLink,
-    PaginationEllipsis
-    , PaginationNext
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationLink,
+  PaginationEllipsis,
+  PaginationNext,
 } from "@/components/ui/pagination";
-import {Button} from "@/components/ui/button";
-import {Checkbox} from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 
 interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
 }
 
 /**
  * 带分页的数据表格
  */
-export function DataTable<TData, TValue>({columns, data}: DataTableProps<TData, TValue>) {
-    //往第一行放入勾选框
-    columns.unshift({
-        id: "select",
-        header: ({table}) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({row}) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    })
-    //最后一列放"查看详情"按钮
-    columns.push(
-        {
-            id: "operate",
-            header: () => (
-                <Button variant="secondary">
-                    <Plus/>
-                </Button>
-            ),
-            cell: () => (
-                <Button variant='ghost'>
-                    <Ellipsis/>
-                </Button>
-            )
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  //往第一行放入勾选框
+  columns.unshift({
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
         }
-    )
-    const table = useReactTable(
-        {
-            data,
-            columns,
-            getCoreRowModel: getCoreRowModel()
-        }
-    );
-    return (
-        <div>
-            <div className="flex justify-center items-center mx-3 my-5">
-                <div className="items-center mr-2">
-                    <LucideScanSearch/>
-                </div>
-                <Input placeholder="查找..." className="max-w-3/5"/>
-            </div>
-            <div className="overflow-hidden rounded-sm border border-gray-200">
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id} className="border-b-gray-200">
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead key={header.id} className="align-middle">
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef.header,
-                                                    header.getContext(),
-                                                )}
-                                        </TableHead>
-                                    );
-                                })}
-                            </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                    className="border-b-gray-200"
-                                >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell colSpan={columns.length} className="h-24 text-center">
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div>
-            <Pagination className="mt-2">
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious/>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink> 1 </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink> 2 </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink> 3 </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationEllipsis/>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationNext/>
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  });
+  //最后一列放"查看详情"按钮
+  columns.push({
+    id: "operate",
+    header: () => (
+      <Button variant="secondary">
+        <Plus />
+      </Button>
+    ),
+    cell: () => (
+      <Button variant="ghost">
+        <Ellipsis />
+      </Button>
+    ),
+  });
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  return (
+    <div>
+      <div className="flex justify-center items-center mx-3 my-5">
+        <div className="items-center mr-2">
+          <LucideScanSearch />
         </div>
-    );
+        <Input placeholder="查找..." className="max-w-3/5" />
+      </div>
+      <div className="flex justify-center">
+        <div className="overflow-hidden rounded-sm border border-gray-200 w-9/10 shadow">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id} className="border-b-gray-200">
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id} className="align-middle">
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                    className="border-b-gray-200"
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+      <Pagination className="mt-2">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink> 1 </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink> 2 </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink> 3 </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
 }
--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -1,171 +1,242 @@
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarHeader,
-    SidebarProvider,
-    SidebarTrigger,
-    SidebarGroupLabel,
-    SidebarMenuItem,
-    SidebarMenuButton,
-    SidebarMenuSub,
-    SidebarMenu
-    , SidebarGroupContent
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarGroupLabel,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarMenuSub,
+  SidebarMenu,
+  SidebarGroupContent,
+  SidebarRail,
+  useSidebar,
 } from "@/components/ui/sidebar";
-import {Collapsible, CollapsibleTrigger, CollapsibleContent} from "@/components/ui/collapsible";
 import {
-    AppWindow,
-    BookUser,
-    ChartPie,
-    ChevronRight, LibraryBig, Search,
-    Settings2
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@/components/ui/collapsible";
+import {
+  AppWindow,
+  BookUser,
+  ChartPie,
+  ChevronRight,
+  FileUser,
+  LibraryBig,
+  LogOut,
+  Search,
+  Settings2,
+  UserCog,
 } from "lucide-react";
-import {InputGroup, InputGroupAddon, InputGroupInput} from "@/components/ui/input-group";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 /**
  * 自定义侧边栏
  * @returns 自定义侧边栏
  */
 function CustomSidebar() {
-    return (
-        <Sidebar className="border-r-gray-200">
-            <SidebarHeader>
-                <InputGroup>
-                    <InputGroupInput placeholder='搜索功能'/>
-                    <InputGroupAddon>
-                        <Search/>
-                    </InputGroupAddon>
-                </InputGroup>
-            </SidebarHeader>
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel>运维面板</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            <SidebarMenuItem>
-                                <SidebarMenuButton>
-                                    <ChartPie/>
-                                    概览
-                                </SidebarMenuButton>
-                            </SidebarMenuItem>
-                            <Collapsible defaultOpen className='group/collapsible'>
-                                <SidebarMenuItem>
-                                    <CollapsibleTrigger asChild>
-                                        <SidebarMenuButton>
-                                            <AppWindow/>
-                                            备份管理
-                                            <ChevronRight
-                                                className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90'/>
-                                        </SidebarMenuButton>
-                                    </CollapsibleTrigger>
-                                    <CollapsibleContent>
-                                        <SidebarMenuSub>
-                                            <SidebarMenuItem>
-                                                <SidebarMenuButton>备份任务</SidebarMenuButton>
-                                            </SidebarMenuItem>
-                                            <SidebarMenuItem>
-                                                <SidebarMenuButton>备份文件</SidebarMenuButton>
-                                            </SidebarMenuItem>
-                                            <SidebarMenuItem>
-                                                <SidebarMenuButton>备份记录</SidebarMenuButton>
-                                            </SidebarMenuItem>
-                                        </SidebarMenuSub>
-                                    </CollapsibleContent>
-                                </SidebarMenuItem>
-                            </Collapsible>
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-                <SidebarGroup>
-                    <SidebarGroupLabel>系统管理</SidebarGroupLabel>
-                    <SidebarMenu>
-                        <SidebarMenuItem>
-                            <SidebarMenuButton>
-                                <BookUser/>
-                                用户管理
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                        <Collapsible defaultOpen className='group/collapsible'>
-                            <SidebarMenuItem>
-                                <CollapsibleTrigger asChild>
-                                    <SidebarMenuButton>
-                                        <Settings2/>
-                                        系统设置
-                                        <ChevronRight
-                                            className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90'/>
-                                    </SidebarMenuButton>
-                                </CollapsibleTrigger>
-                                <CollapsibleContent>
-                                    <SidebarMenuSub>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>Maid节点</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>系统配置</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>删库跑路</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    </SidebarMenuSub>
-                                </CollapsibleContent>
-                            </SidebarMenuItem>
-                        </Collapsible>
-                        <Collapsible defaultOpen className='group/collapsible'>
-                            <SidebarMenuItem>
-                                <CollapsibleTrigger asChild>
-                                    <SidebarMenuButton>
-                                        <LibraryBig/>
-                                        文档中心
-                                        <ChevronRight
-                                            className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90'/>
-                                    </SidebarMenuButton>
-                                </CollapsibleTrigger>
-                                <CollapsibleContent>
-                                    <SidebarMenuSub>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>使用说明</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>服务契约</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>保密条款</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>免责声明</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton>开源组件许可证</SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    </SidebarMenuSub>
-                                </CollapsibleContent>
-                            </SidebarMenuItem>
-                        </Collapsible>
-                    </SidebarMenu>
-                </SidebarGroup>
-            </SidebarContent>
-            <SidebarFooter/>
-        </Sidebar>
-    )
-        ;
+  const { isMobile } = useSidebar();
+  return (
+    <Sidebar className="border-r-gray-200">
+      <SidebarHeader>
+        <InputGroup>
+          <InputGroupInput placeholder="搜索功能" />
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+        </InputGroup>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>运维面板</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <ChartPie />
+                  概览
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      <AppWindow />
+                      备份管理
+                      <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                          <a href="/backup/jobs">备份任务</a>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                            <a href="/backup/files">备份文件</a>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                            <a href="/backup/records">备份记录</a>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>系统管理</SidebarGroupLabel>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <BookUser />
+                用户管理
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <Collapsible defaultOpen className="group/collapsible">
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton>
+                    <Settings2 />
+                    系统设置
+                    <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>Maid节点</SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>系统配置</SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>删库跑路</SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+            <Collapsible defaultOpen className="group/collapsible">
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton>
+                    <LibraryBig />
+                    文档中心
+                    <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>使用说明</SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>服务契约</SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>保密条款</SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>免责声明</SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>开源组件许可证</SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton className="px-1 py-0 h-12 ">
+                  <Avatar className="my-6 mx-1">
+                    <AvatarImage src="../../public/favicon.svg" alt="USER" />
+                    <AvatarFallback>LR</AvatarFallback>
+                  </Avatar>
+                  <div className="grid-cols-2 my-3">
+                    <div className="my-0.3">Herobrine</div>
+                    <div className="mt-0.3 mb-1 text-xs">超级用户</div>
+                  </div>
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-[--radix-popper-anchor-width]"
+                side={isMobile ? "bottom" : "right"}
+              >
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel>Herobrine</DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    <FileUser />
+                    用户档案
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <UserCog />
+                    用户设置
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <LogOut />
+                    退出登录
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
 
 /**
  * 面板页面组件，包含一个侧边栏和一个主内容区域
  */
-export function Panel({children}: { children: React.ReactNode }) {
-    return (
-        <SidebarProvider>
-            <CustomSidebar/>
-            <main>
-                <div className="w-8 h-8">
-                    <SidebarTrigger/>
-                </div>
-                <div className="max-w-4/5 justify-center items-center mx-auto">
-                    {children}
-                </div>
-            </main>
-        </SidebarProvider>
-    );
+export function Panel({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <CustomSidebar />
+      <main>
+        <div className="w-8 h-8">
+          <SidebarTrigger />
+        </div>
+        <div className="max-w-4/5 justify-center items-center mx-auto">
+          {children}
+        </div>
+      </main>
+    </SidebarProvider>
+  );
 }
